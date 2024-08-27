05154b58c9a835224be3fed914b90cdf3ff86f1a
Mengyi <mguo@simwell.io>
2024-08-27 17:55:18 -0400

    add gateway

--- gateway/src/main/java/com/mengyicloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/mengyicloud/gateway/GatewayApplication.java
@@ -1,13 +1,13 @@
-package com.mengyicloud.demo;
+package com.mengyicloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
